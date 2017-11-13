48cf1b9d84a333a985740d134c779770277f6150
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-13 20:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ function BoardMember (name, homestate, training){
   this.name = name;
   this.homestate = homestate;
   this.training = trainging;
-  this.sayHello = function(){
-    console.log(`Hello everybody, my name is ${this.name}!`);
+  this.veto = function(){
+    console.log('No, I must disagree');
   }
+  this.approve = function(){
+    console.log('You can do that!');
+  }
+  this.doCharity = function(){
+    console.log('I like to help people.');
+  }
+  
 }
 
 
 
 
-let bobby = new User('bobby', 20, 'Philadelphia')
 
-
-let susan = {
-  name: 'susan',
-  age: 28,
-  hometown: 'Boston'
-}
